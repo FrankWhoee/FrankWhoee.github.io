462c608f418c9e1797c94fc1f637ecabd79db1a2
Frank Hui <huifrank@live.com>
2019-10-05 22:08:28 -0700

    New website lol

--- img/photographs/automater.py
+++
@@ -1,4 +0,0 @@
-import os
-for file in os.listdir("."):
-    print("<div class=\"single-portfolio col-sm-4 all logo\"><div class=\"relative\"><div class=\"thumb\"><div class=\"overlay overlay-bg\"></div><img class=\"image img-fluid\" src=\"img/photographs/"+file+"\" alt=\"\"></div><a href=\"img/p5.jpg\" class=\"img-pop-up\">	<div class=\"middle\"><div class=\"text align-self-center d-flex\"><img src=\"img/preview.png\" alt=\"\"></div></div></a></div><div class=\"p-inner\"><h4></h4><div class=\"cat\">Picture</div></div></div>")
-
