d1d39d98b86ca8a33bc7889b63b9beb30bbb8dbd
Frank Hui <huifrank@live.com>
2022-03-09 00:36:30 -0800

    Add typing animation

---
+++ js/script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new TypeIt("#typingtext", {
+        speed: 50
+    }).type("PROGRAMMER // 01")
+        .pause(1500)
+        .delete(16)
+        .type("DESIGNER // 02")
+        .pause(1500)
+        .delete(14)
+        .type("PHOTOGRAPHER // 03")
+        .pause(1500)
+        .delete(18)
+        .type("GAMER // 04")
+        .pause(1500)
+        .delete(11)
+        .type("SDN // 05")
+        .pause(1500)
+        .delete(9)
+        .type("I AM FRANK HUI.")
+        .go();
+
+
+});
+
+
+anime({
+    targets: '.arrow',
+    translateY: 10,
+    direction: 'alternate',
+    loop: true,
+    easing: 'easeInOutSine'
+});
