7e7c861d4d75ed23a5e2a00a31515ae684cc50cb
Frank Hui <huifrank@live.com>
2022-03-11 13:55:25 -0800

    Add github activity chart

--- js/script.js
+++ js/script.js
@@ -37,3 +37,42 @@ anime({
     easing: 'easeInOutSine'
 });
 
+const ctx = document.getElementById('githubChart');
+
+// https://api.github.com/users/frankwhoee/events?page=1
+data = {}
+fetch("https://api.github.com/users/frankwhoee/events?per_page=100").then(response => response.json()).then(json =>{
+    for(i = 0; i < json.length; i++){
+        stamp = json[i].created_at.substring(0,10)
+        if(stamp in data){
+            data[stamp] += 1
+        }else{
+            data[stamp] = 1
+        }
+    }
+
+    data = Object.entries(data)
+    for(i = 0; i < data.length; i++){
+        data[i] = {x:data[i][0], y:data[i][1]}
+    }
+    console.log(data)
+    let chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            datasets: [{
+                label: 'Github Contributions',
+                data: data,
+                borderColor: "rgba(211,141,95,1)",
+                backgroundColor: "rgba(211,141,95,1)"
+            }],
+        },
+        options: {
+            scales: {
+                x: {
+                    type: 'time'
+                }
+            }
+        }
+    });
+})
+
